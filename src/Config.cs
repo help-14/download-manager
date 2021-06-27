5730ce554308594f76fd72cb218adc08fb61fc31
Help-14 <help-14@hotmail.com>
2021-06-27 15:03:59 +0700

    Add queue

--- src/Config.cs
+++ src/Config.cs
@@ -6,7 +6,11 @@ public static class Config
     public static void Load()
     {
         DownloadPath = Environment.GetEnvironmentVariable("DownloadPath") ?? Directory.GetCurrentDirectory();
+
+        var concurrent = Environment.GetEnvironmentVariable("ConcurrentDownload");
+        ConcurrentDownload = string.IsNullOrEmpty(concurrent) ? 2 : Convert.ToInt32(concurrent);
     }
 
     public static string DownloadPath { get; set; }
+    public static int ConcurrentDownload { get; set; }
 }
--- src/Functions/DownloadQueue.cs
+++ src/Functions/DownloadQueue.cs
@@ -1,12 +1,47 @@
+using System;
 using System.Collections.Generic;
+using System.Timers;
 
 public static class DownloadQueue
 {
     public static PriorityQueue<DownloadClient, int> Queue = new PriorityQueue<DownloadClient, int>();
+    public static List<DownloadClient> Running = new List<DownloadClient>();
+
+    private static Timer checkTimer;
+
+    public static void Start()
+    {
+        if (checkTimer == null)
+        {
+            checkTimer = new Timer(1000);
+            checkTimer.Elapsed += Timer_Elapsed;
+        }
+        checkTimer.Start();
+    }
+
+    private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        if(Running.Count > 0)
+        {
+            for (var i = Running.Count - 1; i >= 0; i--)
+            {
+                var job = Running[i];
+                if (job.Completed) Running.RemoveAt(i);
+                job.Dispose();
+            }
+        }
+        while(Running.Count < Config.ConcurrentDownload)
+        {
+            var job = Queue.Dequeue();
+            Running.Add(job);
+            _ = job.DownloadAsync();
+        }
+    }
 
     public static void Add(DownloadClient client)
     {
         Queue.Enqueue(client, 2);
+        Start();
     }
 
     public static void Add(string url)
